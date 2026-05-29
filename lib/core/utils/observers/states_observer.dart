04e36cdf31e76877ad085dd11fb9e3c4378e2b64
Eng-Izak <eng.izakrammah@gmail.com>
2026-05-29 20:03:52 +0300

    create block observer

--- lib/core/utils/observers/states_observer.dart
+++ lib/core/utils/observers/states_observer.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class StatesObserver implements BlocObserver {
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    debugPrint('onChange -- bloc: ${bloc.runtimeType}, change: $change');
+  }
+
+  @override
+  void onClose(BlocBase<dynamic> bloc) {
+    debugPrint('onClose -- bloc: ${bloc.runtimeType}');
+  }
+
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    // TODO: implement onCreate
+  }
+
+  @override
+  void onDone(
+    Bloc<dynamic, dynamic> bloc,
+    Object? event, [
+    Object? error,
+    StackTrace? stackTrace,
+  ]) {
+    // TODO: implement onDone
+  }
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    // TODO: implement onEvent
+  }
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    // TODO: implement onTransition
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:notes_app_001/core/dependency_injection/dependency_injection.dart';
 import 'package:notes_app_001/core/shared/models/note_model.dart';
 import 'package:notes_app_001/core/utils/constants/key_constants.dart';
+import 'package:notes_app_001/core/utils/observers/states_observer.dart';
 import 'package:notes_app_001/notes_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = StatesObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
